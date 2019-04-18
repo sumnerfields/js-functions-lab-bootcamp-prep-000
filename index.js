7c433c2611351dde175e0701e2e662937f40b1ca
Sumner Fields <sumnertfields@gmail.com>
2019-04-18 13:26:44 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ function happyHolidays() {return 'Happy holidays!'}
 
 function happyHolidaysTo(name) {return "Happy holidays,   ${name} !"}
 
-function happyHolidayTo(holiday, name) {return "Happy " ${holiday} ", " ${name}"!"}
+function happyHolidayTo(holiday, name) {return "Happy  ${holiday}, ${name}!"}
 
-function holidayCountdown(days, holiday) {return "It's " ${days} " days until " ${holiday} "!"}
+function holidayCountdown(days, holiday) {return "It's ${days} days until ${holiday} !"}
